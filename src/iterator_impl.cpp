c93d271cc4d5a3ba6e2cd3881c6c552a5e8c786b
Andeo1812 <alexanforall@gmail.com>
2021-12-07 03:38:18 +0300

    v 0.06 impl iterator in process

--- src/iterator_impl.cpp
+++ src/iterator_impl.cpp
@@ -3,57 +3,59 @@
 namespace task {
     template<class T>
     list<T>::iterator::iterator() {
-
+        it = NULL;
     }
 
     template<class T>
     list<T>::iterator::iterator(const iterator&) {
-
+        *this = iterator;
     }
 
     template<class T>
-    typename list<T>::iterator& list<T>::iterator::operator=(const iterator&){
-
+    typename list<T>::iterator& list<T>::iterator::operator=(const iterator&) {
+        return this = iterator;
     }
 
     template<class T>
     typename list<T>::iterator& list<T>::iterator::operator++() {
-
+        return it->next;
     }
 
     template<class T>
     typename list<T>::iterator list<T>::iterator::operator++(int) {
-
+        it++;
+        return &it;
     }
 
     template<class T>
     typename list<T>::iterator::reference list<T>::iterator::operator*() const {
-
+        return *it;
     }
 
     template<class T>
     typename list<T>::iterator::pointer list<T>::iterator::operator->() const {
-
+        return *this;
     }
 
     template<class T>
     typename list<T>::iterator& list<T>::iterator::operator--() {
-
+        return it->prev;
     }
 
     template<class T>
     typename list<T>::iterator list<T>::iterator::operator--(int) {
-
+            it--;
+        return &it;
     }
 
     template<class T>
     bool list<T>::iterator::operator==(iterator other) const {
-        return false;
+        return *this == other;
     }
 
     template<class T>
     bool list<T>::iterator::operator!=(iterator other) const {
-        return false;
+        return !(*this == other);
     }
 
 
